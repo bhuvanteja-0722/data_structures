7795b852f2e50b510f1db74d71bf05a0d1fbf8b8
bhuvanteja-0722 <bhuvanteja212212@gmail.com>
2025-11-11 14:41:28 +0530

    Add files via upload

---
+++ double ended queue.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <conio.h>
+#define MAX 10
+
+int deque[MAX];
+int left = -1, right = -1;
+
+void insert_right(void);
+void insert_left(void);
+void delete_right(void);
+void delete_left(void);
+void display(void);
+int main()
+{
+    int choice;
+    clrscr();
+    do
+    {
+        printf("\n1.Insert at right ");
+        printf("\n2.Insert at left ");
+        printf("\n3.Delete from right ");
+        printf("\n4.Delete from left ");
+        printf("\n5.Display ");
+        printf("\n6.Exit");
+        printf("\n\nEnter your choice ");
+        scanf("%d", &choice);
+        switch(choice)
+        {
+            case 1: insert_right(); break;
+            case 2: insert_left(); break;
+            case 3: delete_right(); break;
+            case 4: delete_left(); break;
+            case 5: display(); break;
+        }
+    } while(choice != 6);
+    getch();
+    return 0;
+}
+void insert_right()
+{
+    int val;
+    printf("\nEnter the value to be added ");
+    scanf("%d", &val);
+    if ((left == 0 && right == MAX - 1) || (left == right + 1))
+    {
+        printf("\nOVERFLOW");
+        return;
+    }
+    if (left == -1)
+    {
+        left = 0;
+        right = 0;
+    }
+    else if (right == MAX - 1)
+        right = 0;
+    else
+        right++;
+    deque[right] = val;
+}
+
+void insert_left()
+{
+    int val;
+    printf("\nEnter the value to be added ");
+    scanf("%d", &val);
+    if ((left == 0 && right == MAX - 1) || (left == right + 1))
+    {
+        printf("\nOVERFLOW");
+        return;
+    }
+    if (left == -1)
+    {
+        left = 0;
+        right = 0;
+    }
+    else if (left == 0)
+        left = MAX - 1;
+    else
+        left--;
+    deque[left] = val;
+}
+void delete_right()
+{
+    if (left == -1)
+    {
+        printf("\nUNDERFLOW");
+        return;
+    }
+    printf("\nThe deleted element is %d\n", deque[right]);
+    if (left == right)
+    {
+        left = -1;
+        right = -1;
+    }
+    else if (right == 0)
+        right = MAX - 1;
+    else
+        right--;
+}
+
+void delete_left()
+{
+    if (left == -1)
+    {
+        printf("\nUNDERFLOW");
+        return;
+    }
+    printf("\nThe deleted element is %d\n", deque[left]);
+    if (left == right)
+    {
+        left = -1;
+        right = -1;
+    }
+    else if (left == MAX - 1)
+        left = 0;
+    else
+        left++;
+}
+void display()
+{
+    int i;
+    if (left == -1)
+    {
+        printf("\nDeque is empty");
+        return;
+    }
+    printf("\nDeque elements are:\n");
+    i = left;
+    while (1)
+    {
+        printf("%d ", deque[i]);
+        if (i == right)
+            break;
+        i = (i + 1) % MAX;
+    }
+    printf("\n");
+}
